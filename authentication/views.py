462d25db17fecdadf5c5e98348825248c319c691
Hasan-TechNinja <hasantechninja@gmail.com>
2025-02-05 16:04:45 +0600

    upload

---
+++ authentication/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import logout, login
+
+# Create your views here.
+
+def Registration(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+
+    context = {
+        'form':form
+    }
+    return render(request, 'registration.html', context)
+
+def LoginView(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request.POST)
+        if form.is_valid():
+            user = form.get_user
+            login(request, user)
+            return redirect('home')
+        else:
+            return redirect('login')
+    else:
+        form = AuthenticationForm()
+    context = {
+        'form':form
+    }
+    return render(request, 'login.html', context)
+
+
+def LogoutView(request):
+    logout(request)
+    return redirect('login')
