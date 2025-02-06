861910d9cfd9994bc99e30eb0f1df3b0a61564cd
Hasan-TechNinja <hasantechninja@gmail.com>
2025-02-06 11:36:18 +0600

    update

--- main/models.py
+++ main/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    create_post = models.TextField(default='Write your post here...')
+    image = models.ImageField(upload_to='post/', blank=True)
+    video = models.FileField(upload_to='video/', blank=True)
+    created = models.DateTimeField(auto_now=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.user.username}, '{self.status}, at {self.updated}"
--- main/views.py
+++ main/views.py
@@ -1,5 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from . forms import PostForm
+from datetime import date, time
+from . models import Post
 
 # Create your views here.
 def HomeView(request):
-    return render(request, 'home.html')
+    post = Post.objects.all()
+    
+    context = {
+        'post':post
+    }
+    return render(request, 'home.html', context)
+
+
+def Create(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user
+            post.created = date.today
+            post.updated = date.today
+            post.save()
+            return redirect('home')
+    else:
+        form = PostForm()
+    context = {
+        'form':form
+    }
+            
+    return render(request, 'create.html', context)
